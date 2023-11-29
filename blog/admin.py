74e2baf90e374c123e884efaf7824c1c724f1817
Piotrus <piotrus@gmail.com>
2023-11-29 21:05:24 +0100

    Utworzenie bazy danych i modeli, aktualizacja kodu

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Tag, Autor, Post
 
-# Register your models here.
+# haslo123
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("autor", "hasztagi", "data")
+    list_display = ("tytul", "data", "autor")
+    prepopulated_fields = {"slug": ("tytul",)}
+
+
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Autor)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Tag(models.Model):
+    hasztag = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.hasztag
+
+    class Meta:
+        verbose_name_plural = "Tagi"
+
+
+class Autor(models.Model):
+    imie = models.CharField(max_length=100)
+    nazwisko = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def full_name(self):
+        return f"{self.imie} {self.nazwisko}"
+
+    def __str__(self):
+        return self.full_name()
+
+    class Meta:
+        verbose_name_plural = "Autorzy"
+
+
+class Post(models.Model):
+    tytul = models.CharField(max_length=150)
+    zawartosc = models.CharField(max_length=200)
+    tresc = models.TextField(validators=[MinLengthValidator(10)])
+    nazwa_obrazka = models.CharField(max_length=100)
+    data = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    hasztagi = models.ManyToManyField(Tag, null=True)
+    autor = models.ForeignKey(
+        Autor, on_delete=models.SET_NULL, null=True, related_name="posty")
+
+    class Meta:
+        verbose_name_plural = "Posty"
